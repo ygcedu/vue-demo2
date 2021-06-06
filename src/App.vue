da2510f2a369b11d875f2fdb45625d7f0879fa27
ygc <937601471@qq.com>
2021-06-06 22:10:38 +0800

    v-model双向绑定

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="message" placeholder="editme"/>
+    <p>Message is: {{ message }}</p>
+    <p>
+      <button @click="message = 'frank'">set message to frank</button>
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      message: 'hi'
+    }
+  },
+  components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
