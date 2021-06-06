30e2a8e79ede9e7c4bdf61d8d353cd2633b7db35
ygc <937601471@qq.com>
2021-06-06 22:21:52 +0800

    checkbox双向绑定到数组

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
+    爱好：{{x}}
     <label>
-      <input type="checkbox" v-model="x"/>
-      <span>x:{{ x }}</span>
+      <input type="checkbox" v-model="x" :value="1"/>
+      <span>抽烟</span>
+    </label>
+    <label>
+      <input type="checkbox" v-model="x" :value="2"/>
+      <span>喝酒</span>
+    </label>
+    <label>
+      <input type="checkbox" v-model="x" :value="3"/>
+      <span>烫头</span>
     </label>
   </div>
 </template>
@@ -12,7 +21,7 @@ export default {
   name: 'App',
   data() {
     return {
-      x: true
+      x: []
     }
   },
   components: {}
