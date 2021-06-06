724cb44da74de88def6c2bf0fb9c08fd25782e96
ygc <937601471@qq.com>
2021-06-06 22:49:10 +0800

    v-model.number检查数字输入类型

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <form @submit.prevent="onSubmit">
       <label>
         <span>用户名</span>
-        <input type="text" v-model.lazy="user.username"/>
+        <input type="text" v-model.number="user.username"/>
       </label>
       <label>
         <span>密码</span>
@@ -23,7 +23,7 @@ export default {
     return {
       x: [],
       user: {
-        username: '',
+        username: 0,
         password: ''
       }
     }
